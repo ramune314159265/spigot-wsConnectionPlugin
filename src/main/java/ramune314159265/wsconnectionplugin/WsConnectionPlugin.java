46ecd938b469b6d8dd6d46c1d7a03be70a84f80b
ramune314159265 <ramune314159265@gmail.com>
2024-03-12 23:09:34 +0900

    リスナ追加

--- src/main/java/ramune314159265/wsconnectionplugin/WsConnectionPlugin.java
+++ src/main/java/ramune314159265/wsconnectionplugin/WsConnectionPlugin.java
@@ -1,5 +1,8 @@
 package ramune314159265.wsconnectionplugin;
 
+import org.bukkit.Bukkit;
+import ramune314159265.wsconnectionplugin.WsPluginListener;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WsConnectionPlugin extends JavaPlugin {
@@ -7,7 +10,7 @@ public final class WsConnectionPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new WsPluginListener(), this);
     }
 
     @Override
---
+++ src/main/java/ramune314159265/wsconnectionplugin/WsPluginListener.java
@@ -0,0 +1,12 @@
+package ramune314159265.wsconnectionplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
+
+public class WsPluginListener implements Listener {
+	@EventHandler
+	public void onServerLoad(ServerLoadEvent event){
+
+	}
+}
