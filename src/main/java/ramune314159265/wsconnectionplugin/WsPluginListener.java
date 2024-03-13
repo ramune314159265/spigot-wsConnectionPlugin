cb8f217fa587b287cba32ffcf5c83d913a18f619
ramune314159265 <ramune314159265@gmail.com>
2024-03-13 10:51:22 +0900

    deathイベント追加

--- src/main/java/ramune314159265/wsconnectionplugin/WsPluginListener.java
+++ src/main/java/ramune314159265/wsconnectionplugin/WsPluginListener.java
@@ -2,14 +2,22 @@ package ramune314159265.wsconnectionplugin;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.server.ServerLoadEvent;
 import ramune314159265.wsconnectionplugin.events.Event;
+import ramune314159265.wsconnectionplugin.events.PlayerDiedEvent;
 import ramune314159265.wsconnectionplugin.events.ServerStartedEvent;
 
 public class WsPluginListener implements Listener {
 	@EventHandler
 	public void onServerLoad(ServerLoadEvent event){
 		Event eventDataToSend = new ServerStartedEvent();
+		WsConnectionPlugin.wsConnection.sendEventData(eventDataToSend);
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event){
+		Event eventDataToSend = new PlayerDiedEvent(event.getEntity().getName(),event.getDeathMessage());
 		WsConnectionPlugin.wsConnection.sendEventData(eventDataToSend);
 	}
 }
---
+++ src/main/java/ramune314159265/wsconnectionplugin/events/PlayerDiedEvent.java
@@ -0,0 +1,16 @@
+package ramune314159265.wsconnectionplugin.events;
+
+import ramune314159265.wsconnectionplugin.WsConnectionPlugin;
+
+public class PlayerDiedEvent extends Event{
+	public String serverId;
+	public String playerId;
+	public String reason;
+
+	public PlayerDiedEvent(String playerId,String reason) {
+		this.type = "server_started";
+		this.serverId = WsConnectionPlugin.serverId;
+		this.playerId = playerId;
+		this.reason = reason;
+	}
+}
