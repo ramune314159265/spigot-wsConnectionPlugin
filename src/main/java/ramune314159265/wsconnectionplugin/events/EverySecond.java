0f92df205c9e6aeac34d33154424cfdc8474f25a
ramune314159265 <ramune314159265@gmail.com>
2024-03-16 22:48:51 +0900

    eventypeが含まれていなかったのを修正

--- src/main/java/ramune314159265/wsconnectionplugin/events/EverySecond.java
+++ src/main/java/ramune314159265/wsconnectionplugin/events/EverySecond.java
@@ -7,6 +7,7 @@ public class EverySecond extends Event{
 	public int tps;
 	public long lastTickTimestamp;
 	public EverySecond(int tps, long lastTickTimestamp){
+		this.type = "every_second_info_send";
 		this.tps = tps;
 		this.lastTickTimestamp = lastTickTimestamp;
 		this.serverId = WsConnectionPlugin.serverId;
