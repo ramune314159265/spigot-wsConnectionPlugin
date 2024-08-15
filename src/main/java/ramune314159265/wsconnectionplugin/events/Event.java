3a4518785a5fab2560c46107b17ed4617274a3c4
ramune314159265 <ramune314159265@gmail.com>
2024-08-15 23:17:02 +0900

    uuidを送るように

--- src/main/java/ramune314159265/wsconnectionplugin/events/Event.java
+++ src/main/java/ramune314159265/wsconnectionplugin/events/Event.java
@@ -1,12 +1,15 @@
 package ramune314159265.wsconnectionplugin.events;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Event {
 	public String type;
 	public Long timestamp;
+	public String uuid;
 
 	public Event() {
 		this.timestamp = new Date().getTime();
+		this.uuid = UUID.randomUUID().toString();
 	}
 }
